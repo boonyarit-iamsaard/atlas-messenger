8fe6371542f1c1b5594d34e5bcfbde1b583fd4c3
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-11-16 20:03:19 +0700

    style: change layout footer text color

--- src/components/layout/LayoutFooter.tsx
+++ src/components/layout/LayoutFooter.tsx
@@ -5,7 +5,7 @@ import { Container, Grid, Link } from '@chakra-ui/react';
 const LayoutFooter: FC = () => {
   return (
     <Grid as="footer" alignItems="center">
-      <Container fontSize="sm" maxW="3xl" textAlign="center">
+      <Container color="gray.400" fontSize="sm" maxW="3xl" textAlign="center">
         <Link
           href="https://dev.to/devteam/announcing-the-mongodb-atlas-hackathon-2022-on-dev-2107"
           target="_blank"
