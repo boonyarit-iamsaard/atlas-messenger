3b97cb86e2cec2f3599a1ab02b6ad606cdb8136f
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2022-11-17 16:22:44 +0700

    fix: basename trailing slash

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/atlas-messenger/',
+  base: '/atlas-messenger',
   plugins: [react()],
 });
